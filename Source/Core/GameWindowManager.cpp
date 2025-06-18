d1972690e935ab7f5294213982ebf11f1cd523bf
Tejas Vasishta G R <wasishta38@gmail.com>
2025-06-19 01:01:17 +0530

    Full Screen

--- Source/Core/GameWindowManager.cpp
+++ Source/Core/GameWindowManager.cpp
@@ -11,7 +11,8 @@ namespace Core
 
 	void GameWindowManager::CreateGameWindow()
 	{
-		gameWindow->create(VideoMode(gameWindowWidth, gameWindowHight), gameTitle);
+		//gameWindow->create(VideoMode(gameWindowWidth, gameWindowHight), gameTitle);
+		gameWindow->create(VideoMode::getDesktopMode(), gameTitle, Style::Fullscreen);
 	}
 
 	bool GameWindowManager::IsGameRunning()
@@ -21,7 +22,11 @@ namespace Core
 
 	void GameWindowManager::render()
 	{
+		gameWindow->clear(Color(200, 50, 50, 255));
 
+		//gameWindow->draw();
+
+		gameWindow->display();
 	}
 
 	RenderWindow* GameWindowManager::GetGameWindow()
